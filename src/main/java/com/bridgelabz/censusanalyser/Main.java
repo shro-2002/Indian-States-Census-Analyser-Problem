0e7f1db6f9196fd15769a7c80ac0eff924762fb5
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-12-19 18:02:02 +0530

    Added Master commit: Welcome to Indian State Census Analyzer

---
+++ src/main/java/com/bridgelabz/censusanalyser/Main.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.censusanalyser;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Indian States Census Analyser Problem!");
+
+	}
+
+}
